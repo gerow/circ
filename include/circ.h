1dd1f09302b9251b95bde3b409bdeed9ebc1c896
Mike Gerow <gerow@mgerow.com>
2020-06-23 03:21:31 +0000

    math is hard

--- include/circ.h
+++ include/circ.h
@@ -14,25 +14,25 @@ int circ_open(struct circ *c, size_t size);
 int circ_close(struct circ *c);
 
 // Reading funcs.
-inline size_t circ_cnt(struct circ *c) {
+static inline size_t circ_cnt(struct circ *c) {
   return (c->head - c->tail) & (c->size - 1);
 }
 
-inline void *circ_read_ptr(struct circ *c) { return c->buf + c->tail; }
+static inline void *circ_read_ptr(struct circ *c) { return c->buf + c->tail; }
 
-inline void circ_read(struct circ *c, size_t n) {
+static inline void circ_read(struct circ *c, size_t n) {
   c->tail = (c->tail + n) & (c->size - 1);
 }
 
 // Writing funcs.
-inline size_t circ_space(struct circ *c) {
-  return (c->tail - c->head + 1) & (c->size - 1);
+static inline size_t circ_space(struct circ *c) {
+  return (c->tail - (c->head + 1)) & (c->size - 1);
 }
 
-inline void *circ_write_ptr(struct circ *c) { return c->buf + c->head; }
+static inline void *circ_write_ptr(struct circ *c) { return c->buf + c->head; }
 
-inline void circ_write(struct circ *c, size_t n) {
+static inline void circ_write(struct circ *c, size_t n) {
   c->head = (c->head + n) & (c->size - 1);
 }
 
-inline void circ_reset(struct circ *c) { c->head = c->tail = 0; }
+static inline void circ_reset(struct circ *c) { c->head = c->tail = 0; }
